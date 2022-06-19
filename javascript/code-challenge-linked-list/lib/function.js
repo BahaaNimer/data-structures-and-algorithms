b45e86eb055f32e684754862a2339b25a123ba45
BahaaNimer <bahaanimer97@gmail.com>
2022-06-19 13:24:15 +0300

    create a function called ziplist

---
+++ javascript/code-challenge-linked-list/lib/function.js
@@ -0,0 +1,22 @@
+//Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the the zipped list
+const LinkedList = require('./linkedList');
+
+function zipLists(list1, list2) {
+  let currentNode1 = list1.head;
+  let currentNode2 = list2.head;
+  let newList = new LinkedList();
+  while (currentNode1 !== null || currentNode2 !== null) {
+    if(currentNode1 !== null) {
+      newList.append(currentNode1.value);
+      currentNode1 = currentNode1.next;
+    }
+    if(currentNode2 !== null) {
+      newList.append(currentNode2.value);
+      currentNode2 = currentNode2.next;
+    }
+  }
+  return newList;
+}
+
+// eslint-disable-next-line eol-last
+module.exports = zipLists;
