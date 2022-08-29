19844bd4addb4a78d950d6c90b6efdd0d4893825
BahaaNimer <bahaanimer97@gmail.com>
2022-08-29 13:17:58 +0300

    add function left join

---
+++ javascript/code-challenge-leftJoin/index.js
@@ -0,0 +1,27 @@
+const util = require('util');
+
+const HashMap = require('./HashMap');
+const leftJoin = require('./leftJoin');
+
+let h1 = new HashMap(50);
+let h2 = new HashMap(50);
+
+h1.add('diligent', 'employed');
+h1.add('fond', 'enamored');
+h1.add('guide', 'usher');
+h1.add('outfit', 'garb');
+h1.add('wrath', 'anger');
+
+h2.add('diligent', 'idle');
+h2.add('fond', 'averse');
+h2.add('guide', 'follow');
+h2.add('flow', 'jam');
+h2.add('wrath', 'delight');
+
+console.log('leftJoin :>> ', leftJoin(h1, h2));
+
+// console.log('first :>>', h1);
+// console.log('second :>> ', h2);
+
+let joined = leftJoin(h1, h2);
+console.log(util.inspect(joined, false, null, true));
